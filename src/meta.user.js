ff0a0f35cfc4e35e16d77e86693b0df8c084c41d
7nik <kfiiranet@gmail.com>
2021-09-09 20:36:04 +0300

    Update meta.user.js

--- src/meta.user.js
+++ src/meta.user.js
@@ -3,6 +3,9 @@
 // @namespace    7nik
 // @version      1.0
 // @description  Adds a calculator to google home page
+// @homepageURL  https://github.com/7nik/test-svelte-userscript
+// @supportURL   https://github.com/7nik/test-svelte-userscript/issues
+// @downloadURL  https://github.com/7nik/test-svelte-userscript/releases/latest/download/userscript.user.js
 // @author       7nik
 // @match        https://www.google.com/
 // @grant        none
